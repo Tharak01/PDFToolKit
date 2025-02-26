1a8d01bca9f4115908573caaace57c623d2db7bc
Tharakeshavan Parthasarathy <ptharak01@gmail.com>
2025-02-27 01:19:11 +0800

    name change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pdf-toolkit",
+    name="Python-pdf-toolkit",
     version="0.1.0",
     author="Tharakeshavan Parthasarathy",
     author_email="Ptharak01@gmail.com",
